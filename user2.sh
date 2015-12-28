030510af132e6e785d7427fceb7b52df51342623
Martin Grieve <jrodservers@gmail.com>
2015-12-28 09:31:01 -0500

    Fixed new user script AKA how do i variable pls

--- user2.sh
+++ user2.sh
@@ -7,7 +7,6 @@
 # Password is set without quotes. Put two double quotes to negate password
 # To skip setup assistant: ~/Library/Preferences/com.apple.SetupAssistant.plist
 
-# NOTE: This script (user2.sh) is currently broken. It seems I do not understand bash variables
 
 USERNAME="test"
 DISPLAYNAME="Test"
@@ -19,15 +18,15 @@ LastID=`dscl . -list /Users UniqueID | awk '{print $2}' | sort -n | tail -1`
 NextID=$((LastID + 1))
 
 . /etc/rc.common
-dscl . create /Users/${USERNAME}
-dscl . create /Users/${USERNAME} RealName ${DISPLAYNAME}
-dscl . create /Users/${USERNAME} hint "None"
-dscl . create /Users/${USERNAME} picture "/Library/User Pictures/Fun/Chalk.tif"
-dscl . passwd /Users/${USERNAME} ${PASSWORD}
-dscl . create /Users/${USERNAME} UniqueID ${NextID}
-dscl . create /Users/${USERNAME} PrimaryGroupID 20
-dscl . create /Users/${USERNAME} UserShell /bin/bash
-dscl . create /Users/${USERNAME} NFSHome/Directory /Users/${USERNAME}
-cp -R /System/Library/User\ Template/English.lproj /Users/${USERNAME}
-chown -R ${USERNAME}:staff /Users/${USERNAME}
+dscl . create /Users/"$USERNAME"
+dscl . create /Users/"$USERNAME" RealName "$DISPLAYNAME"
+dscl . create /Users/"$USERNAME" hint "None"
+dscl . create /Users/"$USERNAME" picture "/Library/User Pictures/Fun/Chalk.tif"
+dscl . passwd /Users/"$USERNAME" "$PASSWORD"
+dscl . create /Users/"$USERNAME" UniqueID "$NextID"
+dscl . create /Users/"$USERNAME" PrimaryGroupID 20
+dscl . create /Users/"$USERNAME" UserShell /bin/bash
+dscl . create /Users/"$USERNAME" NFSHome/Directory /Users/"$USERNAME"
+cp -R /System/Library/User\ Template/English.lproj /Users/"$USERNAME"
+chown -R "$USERNAME":staff /Users/"$USERNAME"
 
