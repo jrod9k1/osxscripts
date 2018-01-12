93ea92fe5594ecd48cfa6d363c845b997ec1804a
Gerard Russell <gr@jrod.sh>
2018-01-12 12:23:16 -0500

    Removed redundant jawn

--- user.sh
+++ user.sh
@@ -8,20 +8,25 @@
 # To skip setup assistant: ~/Library/Preferences/com.apple.SetupAssistant.plist
 
 
+USERNAME="test"
+DISPLAYNAME="Test"
+PASSWORD=""
+
+# You should not modify anything below this line
 
 LastID=`dscl . -list /Users UniqueID | awk '{print $2}' | sort -n | tail -1`
 NextID=$((LastID + 1))
 
 . /etc/rc.common
-dscl . create /Users/access
-dscl . create /Users/access RealName Access
-dscl . create /Users/access hint "None"
-dscl . create /Users/access picture "/Library/User Pictures/Fun/Chalk.tif"
-dscl . passwd /Users/access ""
-dscl . create /Users/access UniqueID $NextID
-dscl . create /Users/access PrimaryGroupID 20
-dscl . create /Users/access UserShell /bin/bash
-dscl . create /Users/access NFSHome/Directory /Users/access
-cp -R /System/Library/User\ Template/English.lproj /Users/access
-chown -R access:staff /Users/access
-reboot
+dscl . create /Users/"$USERNAME"
+dscl . create /Users/"$USERNAME" RealName "$DISPLAYNAME"
+dscl . create /Users/"$USERNAME" hint "None"
+dscl . create /Users/"$USERNAME" picture "/Library/User Pictures/Fun/Chalk.tif"
+dscl . passwd /Users/"$USERNAME" "$PASSWORD"
+dscl . create /Users/"$USERNAME" UniqueID "$NextID"
+dscl . create /Users/"$USERNAME" PrimaryGroupID 20
+dscl . create /Users/"$USERNAME" UserShell /bin/bash
+dscl . create /Users/"$USERNAME" NFSHome/Directory /Users/"$USERNAME"
+cp -R /System/Library/User\ Template/English.lproj /Users/"$USERNAME"
+chown -R "$USERNAME":staff /Users/"$USERNAME"
+
